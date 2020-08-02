92506e3f6bd49d1ca9c583d633dd81a778c952d1
DeltaZero <233a344a455@gmail.com>
2020-08-02 20:05:18 +0800

    fix bias

--- simple_train/my_train.py
+++ simple_train/my_train.py
@@ -35,7 +35,8 @@ class FullConnectedLayer:
 
         d_prev_inp = np.sum(self.weight * grad, axis=0, keepdims=True).T  # prev_output的导数，用于上传梯度
         d_weight = self.prev_input.repeat(self.out_features, axis=1).T * grad.repeat(self.in_features, axis=1)  # weight矩阵的偏导
-        d_bias = grad * self.bias
+        d_bias = grad
+        # print("bias=%s, grad=%s" %(d_bias, grad))
 
         self.weight -= d_weight * step_w
         self.bias -= d_bias * step_b
@@ -75,48 +76,48 @@ class SigmoidLayer():
 
 
 if __name__ == "__main__":
-    fc1 = FullConnectedLayer(1, 10)
-    fc2 = FullConnectedLayer(10, 1)
-    sig = SigmoidLayer(10)
+    fc1 = FullConnectedLayer(1, 1)
+    # fc2 = FullConnectedLayer(3, 1)
+    sig = SigmoidLayer(3)
     mesloss = MSELoss()
 
     loss = []
     i = 0
     for epoch in range(3000):
-        x = np.linspace(-5, 5, 50)
-        y =  4*x#np.sin(x) + np.random.randn(50)
+        x = np.random.randn(50) * 3
+        y =  4*x-10#np.sin(x) + np.random.randn(50)
 
         x = np.expand_dims(np.expand_dims(x, -1), -1)
         y = np.expand_dims(np.expand_dims(y, -1), -1)
 
         for i, (x_, y_) in enumerate(zip(x, y)):
             t = fc1.forward(x_)
-            t = sig.forward(t)
-            t = fc2.forward(t)
+            # t = sig.forward(t)
+            # t = fc2.forward(t)
             t = mesloss.forward(t, y_)
             loss.append(t)
             t = mesloss.backward(y_)
-            t = fc2.backward(t, 1e-3, 1e-3)
-            t = sig.backward(t)
+            # t = fc2.backward(t, 1e-3, 1e-3)
+            # t = sig.backward(t)
             t = fc1.backward(t, 1e-3, 1e-3)
 
 
         x = np.linspace(-5, 5, 50)
-        y =  4*x#np.sin(x)
+        y =  4*x-10#np.sin(x)
 
         y_ = []
         x_ = np.expand_dims(np.expand_dims(x, -1), -1)
         for xx in x_:
             # print('a %s' %fc2.forward(fc1.forward(xx)))
-            y_.append(fc2.forward(fc1.forward(xx)).flatten().item())
-            # y_.append(fc1.forward(xx).flatten().item())
+            # y_.append(fc2.forward(fc1.forward(xx)).flatten().item())
+            y_.append(fc1.forward(xx).flatten().item())
         
         # print(y_)
-        # plt.plot(x, y, color='green')
-        # plt.plot(x, y_, color='red')
-        if epoch % 5 == 0:
-            plt.clf()
-            plt.plot(loss)
+        plt.plot(x, y, color='green')
+        plt.plot(x, y_, color='red')
+        # if epoch % 5 == 0:
         plt.pause(0.1)
+        plt.clf()
+        # plt.plot(loss)
 
 # plt.show()
