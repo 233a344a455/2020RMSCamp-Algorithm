e2a63834209936ccfa1895f0007c7ecd45fe14e7
DeltaZero <233a344a455@gmail.com>
2020-08-02 15:17:20 +0800

    update

--- simple_train/my_train.py
+++ simple_train/my_train.py
@@ -12,13 +12,15 @@ class FullConnectedLayer:
         self.in_features, self.out_features = in_features, out_features
         self.bias = np.random.randn(out_features, 1) * 0.1
         self.weight = np.random.randn(out_features, in_features) * 0.1
+        self.prev_input = None
 
     def forward(self, input):
         """正向传播
         """
+        self.prev_input = input
         return np.dot(self.weight, input) + self.bias
 
-    def backward(self, grad, prev_output, step_w, step_b):
+    def backward(self, grad, step_w, step_b):
         """反向传播 The headacheing backward func
 
         Args:
@@ -32,19 +34,49 @@ class FullConnectedLayer:
 
         """
         prev_output_deri = np.sum(self.weight, axis=1, keepdims=True)  # prev_output的导数，用于上传梯度
-        weight_deri = prev_output.repeat(self.out_features, axis=1).T   # weight矩阵的偏导
+        weight_deri = self.prev_input.repeat(self.out_features, axis=1).T   # weight矩阵的偏导
         self.weight += weight_deri * grad.repeat(self.in_features, axis=1) * step_w
         self.bias += grad * np.sum(self.bias, axis=1, keepdims=True) * step_b
 
         return prev_output_deri * grad
 
+class MSELoss():
+    def __init__(self, in_features):
+        self.in_features = in_features
+        self.prev_input = None
+    
+    def forward(self, input, target):
+        """
 
+        Args:
+            input(np.array): shape=(1, in_features)
+
+        """
+        self.prev_input = input
+        return np.mean((input - target) ** 2)
+
+    def backward(self, loss):
+        return 2 * (self.prev_input - loss)
+        
+        
 
 
 if __name__ == "__main__":
-    x = np.linspace(-1, 1, 50)
-    y = x * 2.3 + np.random.randn(50)
-
     fc = FullConnectedLayer(1, 1)
-    for x_, y_ in zip(x, y):
-        np.expand_dims
+    fc.weight[0, 0] = 0
+    fc.bias[0, 0] = 0
+    print(fc.bias, fc.weight)
+    # mesloss = MSELoss(10)
+    # for _ in range(2):
+    #     x = np.linspace(-100, 100, 50)
+    #     y = x * 2.3 + np.random.randn(50)
+
+    #     x = np.expand_dims(np.expand_dims(x, -1), -1)
+    #     y = np.expand_dims(np.expand_dims(y, -1), -1)
+
+    #     for x_, y_ in zip(x, y):
+    #         t = fc.forward(x_)
+    #         t = mesloss.forward(t, y_)
+    #         print(t)
+    #         t = mesloss.backward(t)
+    #         t = fc.backward(t, 0.0001, 0.001)
