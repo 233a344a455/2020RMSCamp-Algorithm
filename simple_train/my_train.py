915c9582d4c186e3ed3dbd0c088be5eae68ad864
DeltaZero <233a344a455@gmail.com>
2020-08-03 12:29:45 +0800

    net fin

--- simple_train/my_train.py
+++ simple_train/my_train.py
@@ -3,33 +3,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 from simple_net import *
 
-fc1 = FullConnectedLayer(1, 20)
-fc2 = FullConnectedLayer(20, 1)
-sig = SigmoidLayer(20)
-loss_fn = MSELoss()
+net = SimpleNet(MSELoss, layers=[
+    FullConnectedLayer(1, 5, 1e-4),
+    LeakyReLULayer(0.05),
+    FullConnectedLayer(5, 5, 1e-4),
+    LeakyReLULayer(0.03),
+    FullConnectedLayer(5, 1, 1e-4)
+])
 
 plt.ion()
+z = 0
 for epoch in range(200):
     x = np.linspace(-20, 20)
-    y = np.sin(x * 0.2) + np.random.randn(50) * 0.2
+    y = x ** 3 * 0.01 + 5 + np.random.randn(50) * 2
     for x_, y_ in random.sample(list(zip(x, y)), 50):
 
-        x = np.linspace(-80, 80, 200)
-        y = map(lambda x_: fc2.forward(sig.forward(fc1.forward(x_))),
-                [np.array([[x_]]) for x_ in x])
-        y = [y_.item() for y_ in y]
+        x_, y_ = np.array([[x_]]), np.array([[y_]])
+        loss = net.train(x_, y_)
+        # pred_ = fc2.forward(relu.forward(fc1.forward(x_)))
+        # loss = loss_fn.forward(pred_, y_)
+        # grad = loss_fn.backward(y_)
+        # grad = fc2.backward(grad, 1*1e-4)
+        # grad = relu.backward(grad)
+        # grad = fc1.backward(grad, 1*1e-4)
 
-        # print(x, y)
-        plt.clf()
-        plt.plot(x, y, color='red')
-        plt.plot(x, np.sin(x*0.2), color='green')
-        # plt.show()
-        plt.pause(0.001)
+        z += 1
+        if z % 10 == 0:
+            print(loss)
+            x = np.linspace(-20, 20, 200)
+            y = map(lambda x_: net.predict(x_),
+                    [np.array([[x_]]) for x_ in x])
+            y = [y_.item() for y_ in y]
 
-        x_ = np.array([[x_]])
-        pred_ = fc2.forward(sig.forward(fc1.forward(x_)))
-        loss = loss_fn.forward(pred_, y_)
-        grad = loss_fn.backward(y_)
-        grad = fc2.backward(grad, 5*1e-4, 5*1e-4)
-        grad = sig.backward(grad)
-        grad = fc1.backward(grad, 5*1e-4, 5*1e-4)
+            # print(x, y)
+            plt.clf()
+            plt.plot(x, y, color='red')
+            plt.plot(x, x**3 * 0.01 + 5, color='green')
+            # plt.show()
+            plt.pause(0.01)
+
+
+
+
--- simple_train/simple_net.py
+++ simple_train/simple_net.py
@@ -1,78 +1,93 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+class SimpleNet():
+    def __init__(self, loss_func, layers=[]):
+        self.layers = layers
+        self.loss_func = loss_func
+
+    def predict(self, data):
+        for layer in self.layers:
+            data = layer.forward(data)
+        return data
+
+    def train(self, data, target):
+        loss, grad = self.loss_func(self.predict(data), target)
+        for layer in reversed(self.layers):
+            grad = layer.backward(grad)
+        return grad
+
+
 class FullConnectedLayer:
-    def __init__(self, in_features, out_features):
+    def __init__(self, in_features, out_features, learning_rate):
         self.in_features, self.out_features = in_features, out_features
+        self.learning_rate = learning_rate
         self.bias = np.random.randn(out_features, 1) * 0.1
         self.weight = np.random.randn(out_features, in_features) * 0.1
-        self.prev_input = None
+        self.prev_inp = None
 
-    def forward(self, input):
+    def forward(self, inp):
         """正向传播
         """
-        self.prev_input = input
-        return np.dot(self.weight, input) + self.bias
+        self.prev_inp = inp
+        return np.dot(self.weight, inp) + self.bias
 
-    def backward(self, grad, step_w, step_b):
+    def backward(self, grad):
         """反向传播 The headacheing backward func
 
         Args:
             grad(np.array): 梯度数据(Z_curr)，shape=(1, out_features)
-            prev_input(np.array): 前一层的输出(A_prev), shape=(1, in_features)
-            step_w(float): 权重步进率
-            step_b(float): 截距步进率
+            prev_inp(np.array): 前一层的输出(A_prev), shape=(1, in_features)
 
         Returns:
             (np.array): 传给上一层的梯度
 
         """
-        # m = self.prev_input.shape[1]
-        # d_weight = np.dot(grad, self.prev_input.T) / m
+        # m = self.prev_inp.shape[1]
+        # d_weight = np.dot(grad, self.prev_inp.T) / m
         # d_bias = np.sum(grad, axis=1, keepdims=True) / m
-        # d_prev_inp = np.dot(self.weight.T, self.prev_input)
+        # d_prev_inp = np.dot(self.weight.T, self.prev_inp)
 
         d_prev_inp = np.sum(self.weight * grad, axis=0, keepdims=True).T  # prev_output的导数，用于上传梯度
-        d_weight = self.prev_input.repeat(self.out_features, axis=1).T * grad.repeat(self.in_features, axis=1)  # weight矩阵的偏导
+        d_weight = self.prev_inp.repeat(self.out_features, axis=1).T * grad.repeat(self.in_features, axis=1)  # weight矩阵的偏导
         d_bias = grad
         # print("bias=%s, grad=%s" %(d_bias, grad))
 
-        self.weight -= d_weight * step_w
-        self.bias -= d_bias * step_b
+        self.weight -= d_weight * self.learning_rate
+        self.bias -= d_bias * self.learning_rate
 
         return d_prev_inp
 
-class MSELoss():
-    def __init__(self):
-        # self.in_features = in_features
-        self.prev_input = None
-    
-    def forward(self, input, target):
-        """
-
-        Args:
-            input(np.array): shape=(1, in_features)
-
-        """
-        self.prev_input = input
-        return np.mean((input - target) ** 2)
-
-    def backward(self, target):
-        return 2 * (self.prev_input - target)
+def MSELoss(pred, target):
+    loss = np.mean((pred - target) ** 2)
+    grad = 2 * (pred - target)
+    return loss, grad
 
 class SigmoidLayer():
-    def __init__(self, n_features):
-        self.n_features = n_features
-        self.prev_input = None
+    def __init__(self):
+        self.prev_inp = None
 
-    def forward(self, input):
-        self.prev_input = input
-        return 1/(1+np.exp(-input))
+    def forward(self, inp):
+        self.prev_inp = inp
+        return 1/(1+np.exp(-inp))
     
     def backward(self, grad):
-        sig = 1/(1+np.exp(-grad))
-        return self.prev_input * sig * (1 - sig)
+        sig = 1/(1+np.exp(-self.prev_inp))
+        return grad * sig * (1 - sig)
 
+class LeakyReLULayer():
+    def __init__(self, leak):
+        self.prev_inp = None
+        self.leak = leak
+
+    def forward(self, inp):
+        self.prev_inp = inp
+        return np.maximum(0.01*inp, inp)
+    
+    def backward(self, grad):
+        self.prev_inp[self.prev_inp < 0] = self.leak
+        self.prev_inp[self.prev_inp >= 0] = 1
+        return grad * self.prev_inp
 
 if __name__ == "__main__":
     fc1 = FullConnectedLayer(1, 10)
