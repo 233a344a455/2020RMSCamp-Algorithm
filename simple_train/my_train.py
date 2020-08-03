5419117a4caeffa6c8eab9bddb1269d5da987bda
DeltaZero <233a344a455@gmail.com>
2020-08-04 07:13:45 +0800

    Add optimizer

--- simple_train/my_train.py
+++ simple_train/my_train.py
@@ -3,44 +3,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 from simple_net import *
 
-net = SimpleNet(MSELoss, layers=[
-    FullConnectedLayer(1, 20, 0.3*1e-4),
-    LeakyReLULayer(0.05),
-    FullConnectedLayer(20, 5, 0.3*1e-4),
-    LeakyReLULayer(0.03),
-    FullConnectedLayer(5, 1, 1e-4)
-])
+net = SimpleNet(MSE_loss, BGD(learning_rate=5 * 1e-3),\
+                                layers=[
+                                    FullConnectedLayer(1, 20),
+                                    SigmoidLayer(),
+                                    FullConnectedLayer(20, 5),
+                                    SigmoidLayer(),
+                                    FullConnectedLayer(5, 1)
+                                ])
 
 plt.ion()
 z = 0
-for epoch in range(20000):
-    x = np.linspace(-20, 20)
-    y = np.sin(x*0.3)+3 + np.random.randn(50)
-    for x_, y_ in random.sample(list(zip(x, y)), 50):
+for epoch in range(2000000):
+    x = np.linspace(-20, 20, 50)
+    y = np.sin(x*0.1)+3 + np.random.randn(50)
 
-        x_, y_ = np.array([[x_]]), np.array([[y_]])
-        loss = net.train(x_, y_)
-        # pred_ = fc2.forward(relu.forward(fc1.forward(x_)))
-        # loss = loss_fn.forward(pred_, y_)
-        # grad = loss_fn.backward(y_)
-        # grad = fc2.backward(grad, 1*1e-4)
-        # grad = relu.backward(grad)
-        # grad = fc1.backward(grad, 1*1e-4)
+    loss = net.train(x[:, np.newaxis], y[:, np.newaxis])
 
-        z += 1
-        if z % 500 == 0:
-            print(loss)
-            x = np.linspace(-20, 20, 200)
-            y = map(lambda x_: net.predict(x_),
-                    [np.array([[x_]]) for x_ in x])
-            y = [y_.item() for y_ in y]
+    z += 1
+    if z % 500 == 0:
+        print(loss)
 
-            # print(x, y)
-            plt.clf()
-            plt.plot(x, y, color='red')
-            plt.plot(x, np.sin(x*0.3)+3, color='green')
-            # plt.show()
-            plt.pause(0.01)
+        plt.clf()
+        x = np.linspace(-20, 20, 100)
+        plt.plot(x, net.predict(x[:, np.newaxis]), color='red')
+        plt.plot(x, np.sin(x*0.1)+3, color='green')
+
+        plt.pause(0.01)
 
 
 
--- simple_train/simple_net.py
+++ simple_train/simple_net.py
@@ -2,66 +2,114 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class SimpleNet():
-    def __init__(self, loss_func, layers=[]):
+    def __init__(self, loss_func, optimizer, layers=[]):
         self.layers = layers
         self.loss_func = loss_func
+        self.optimizer = optimizer
+        self.optimizer.link_layers_params(self.layers)
 
-    def predict(self, data):
+    def predict(self, data, return_vec=False):
+        """
+
+        Args:
+            data.shape = (batch, in_features)
+
+        Returns:
+            data.shape = (batch, out_features)
+
+        """
+        data = np.expand_dims(data, axis=-1) # (batch, in_features) -> (batch, in_features, 1)
         for layer in self.layers:
             data = layer.forward(data)
-        return data
+        if return_vec:
+            return data
+        else:
+            return np.squeeze(data, axis=-1) # (batch, out_features, 1) -> (batch, out_features)
 
     def train(self, data, target):
-        loss, grad = self.loss_func(self.predict(data), target)
+        """
+
+        Args:
+            data.shape = (batch, in_features)
+            target.shape = (batch, out_features)
+
+        Returns:
+            loss(float)
+
+        """
+        target = np.expand_dims(target, axis=-1)    # (batch, out_features) -> (batch, out_features, 1)
+        loss, grad = self.loss_func(self.predict(data, return_vec=True), target)
         for layer in reversed(self.layers):
             grad = layer.backward(grad)
-        return grad
+        self.optimizer.step()
+        return loss
 
 
 class FullConnectedLayer:
-    def __init__(self, in_features, out_features, learning_rate):
+    def __init__(self, in_features, out_features):
+        """
+
+        self.weight.shape = (1, out_features, in_features)
+        self.weight.bias = (1, out_features, 1)
+
+        """
         self.in_features, self.out_features = in_features, out_features
-        self.learning_rate = learning_rate
-        self.bias = np.random.randn(out_features, 1) # * 0.1
-        self.weight = np.random.randn(out_features, in_features) # * 0.1
+        self.bias = np.random.randn(1, out_features, 1) # * 0.1
+        self.weight = np.random.randn(1, out_features, in_features) # * 0.1
         self.prev_inp = None
+        self.d_weight = None
+        self.d_bias = None
 
     def forward(self, inp):
         """正向传播
+
+        inp.shape = (batch, in_features, 1)
+
         """
         self.prev_inp = inp
-        return np.dot(self.weight, inp) + self.bias
+        return self.weight @ inp + self.bias
 
     def backward(self, grad):
-        """反向传播 The headacheing backward func
+        """反向传播 
+        The headaching backward func !!!
 
         Args:
-            grad(np.array): 梯度数据(Z_curr)，shape=(1, out_features)
-            prev_inp(np.array): 前一层的输出(A_prev), shape=(1, in_features)
+            grad(np.array): 梯度数据(Z_curr)，shape=(batch, out_features, 1)
+            prev_inp(np.array): 前一层的输出(A_prev), shape=(batch, in_features, 1)
 
         Returns:
             (np.array): 传给上一层的梯度
 
         """
-        # m = self.prev_inp.shape[1]
-        # d_weight = np.dot(grad, self.prev_inp.T) / m
-        # d_bias = np.sum(grad, axis=1, keepdims=True) / m
-        # d_prev_inp = np.dot(self.weight.T, self.prev_inp)
-
-        d_prev_inp = np.sum(self.weight * grad, axis=0, keepdims=True).T  # prev_output的导数，用于上传梯度
-        d_weight = self.prev_inp.repeat(self.out_features, axis=1).T * grad.repeat(self.in_features, axis=1)  # weight矩阵的偏导
-        d_bias = grad
-        # print("bias=%s, grad=%s" %(d_bias, grad))
-
-        self.weight -= d_weight * self.learning_rate
-        self.bias -= d_bias * self.learning_rate
+        d_prev_inp = np.sum(self.weight * grad, axis=1, keepdims=True).transpose(0, 2, 1)  # prev_output的导数，用于上传梯度
+        self.d_weight = self.prev_inp.repeat(self.out_features, axis=-1).transpose(0, 2, 1) * grad.repeat(self.in_features, axis=-1)  # weight矩阵的偏导
+        self.d_bias = grad
 
         return d_prev_inp
 
-def MSELoss(pred, target):
-    loss = np.mean((pred - target) ** 2)
+
+# ========================== Loss Functions ==========================
+
+
+def MSE_loss(pred, target):
+    """
+
+    Args:
+        pred.shape = (batch, n_featuers, 1)
+        target.shape = (batch, n_featuers, 1)
+    
+    Returns:
+        loss (float)
+        gred.shape = (batch, n_featuers, 1)
+
+    """
+    loss = np.mean((pred - target) ** 2).item()
     grad = 2 * (pred - target)
     return loss, grad
+
+
+# ======================== Activation Layers ========================
+
 
 class SigmoidLayer():
     def __init__(self):
@@ -88,3 +136,19 @@ class LeakyReLULayer():
         self.prev_inp[self.prev_inp < 0] = self.leak
         self.prev_inp[self.prev_inp >= 0] = 1
         return grad * self.prev_inp
+
+
+# ========================== Optimizers ==========================
+
+
+class BGD():
+    def __init__(self, learning_rate):
+        self.learning_rate = learning_rate
+    
+    def link_layers_params(self, layers):
+        self.maintaining_layers = [layer for layer in layers if isinstance(layer, FullConnectedLayer)]
+
+    def step(self):
+        for layer in self.maintaining_layers:
+            layer.weight -= np.mean(layer.d_weight, axis=0, keepdims=True) * self.learning_rate
+            layer.bias -= np.mean(layer.d_bias, axis=0, keepdims=True) * self.learning_rate
