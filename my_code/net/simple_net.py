dc0aae78cb723b189cd1080b28491a607e3619b5
DeltaZero <233a344a455@gmail.com>
2020-08-10 18:44:43 +0800

    active batch_norm

--- my_code/net/simple_net.py
+++ my_code/net/simple_net.py
@@ -6,7 +6,7 @@ class SimpleNet():
         self.layers = layers
         self.loss_func = loss_func
         self.optimizer = optimizer
-        self.optimizer.link_layers_params(self.layers)
+        self.optimizer.link_parameters(self.layers)
 
     def predict(self, data, train=False):
         """
@@ -45,6 +45,12 @@ class SimpleNet():
         return loss
 
 
+class Parameter():
+    def __init__(self, value):
+        self.value = value
+        self.dvalue = None
+
+
 class FullConnectedLayer():
     def __init__(self, in_features, out_features):
         """
@@ -54,11 +60,11 @@ class FullConnectedLayer():
 
         """
         self.in_features, self.out_features = in_features, out_features
-        self.bias = np.random.randn(1, out_features, 1) / np.sqrt(in_features / 2)
-        self.weight = np.random.randn(1, out_features, in_features) / np.sqrt(in_features / 2)
+        self.bias = Parameter(np.random.randn(1, out_features, 1) / np.sqrt(in_features / 2))
+        self.weight = Parameter(np.random.randn(1, out_features, in_features) / np.sqrt(in_features / 2))
         self.prev_inp = None
-        self.d_weight = None
-        self.d_bias = None
+        # self.d_weight = None
+        # self.d_bias = None
 
     def forward(self, inp):
         """
@@ -67,7 +73,7 @@ class FullConnectedLayer():
 
         """
         self.prev_inp = inp
-        return self.weight @ inp + self.bias
+        return self.weight.value @ inp + self.bias.value
 
     def backward(self, grad):
         """The headaching backward func !!!
@@ -80,11 +86,11 @@ class FullConnectedLayer():
             (np.array): shape=(batch, in_features, 1)
 
         """
-        d_prev_inp = np.sum(self.weight * grad, axis=1, keepdims=True).transpose(0, 2, 1)  # prev_output的导数，用于上传梯度
-        self.d_weight = self.prev_inp.repeat(self.out_features, axis=-1).transpose(0, 2, 1) * grad.repeat(self.in_features, axis=-1)  # weight矩阵的偏导
-        self.d_bias = grad
-        self.d_weight = np.mean(self.d_weight, axis=0, keepdims=True)
-        self.d_bias = np.mean(self.d_bias, axis=0, keepdims=True)
+        d_prev_inp = np.sum(self.weight.value * grad, axis=1, keepdims=True).transpose(0, 2, 1)  # prev_output的导数，用于上传梯度
+        self.weight.dvalue = self.prev_inp.repeat(self.out_features, axis=-1).transpose(0, 2, 1) * grad.repeat(self.in_features, axis=-1)  # weight矩阵的偏导
+        self.bias.dvalue = grad
+        self.weight.dvalue = np.mean(self.weight.dvalue, axis=0, keepdims=True)
+        self.bias.dvalue = np.mean(self.bias.dvalue, axis=0, keepdims=True)
 
         return d_prev_inp
 
@@ -102,59 +108,59 @@ class DropoutLayer():
         return grad * self.filter
 
 
-# class BatchNormLayer():
-#     def __init__(self, n_features, eps=1e-5, momentum=0.9):
-#         self.eps = eps
-#         self.momentum = momentum
-#         self.n_featuers = n_featuers
+class BatchNormLayer():
+    def __init__(self, n_features, eps=1e-5, momentum=0.9):
+        self.eps = eps
+        self.momentum = momentum
+        self.n_features = n_features
 
-#         self.gamma = np.zeros((n_featuers, 1))
-#         self.beta = np.zeros((n_featuers, 1))
-#         self.running_mean = 0
-#         self.running_var = 0
+        self.gamma =Parameter(np.zeros(n_features))
+        self.beta = Parameter(np.zeros(n_features))
+        self.running_mean = 0
+        self.running_var = 0
         
-#         self.out_ = None
-#         self.prev_inp = None
-#         self.sample_mean = None
-#         self.sample_var = None
-#         self.d_gamma = None
-#         self.d_beta = None
+        self.out_ = None
+        self.prev_inp = None
+        self.sample_mean = None
+        self.sample_var = None
+        # self.d_gamma = None
+        # self.d_beta = None
 
-#     def forward(inp, train):
-#         inp = inp[...,0]
-#         self.prev_inp = inp
+    def forward(self, inp, train=True):
+        inp = inp[...,0]
+        self.prev_inp = inp
         
-#         if train:
-#             self.sample_mean = np.mean(inp, axis=0)
-#             self.sample_var = np.var(inp, axis=0)
-#             self.out_ = (inp - self.sample_mean) / np.sqrt(self.sample_var + self.eps)
-#             self.running_mean = self.momentum * self.running_mean + (1 - self.momentum) * self.sample_mean
-#             self.running_var = self.momentum * self.running_var + (1 - self.momentum) * self.sample_var
-#             out = self.gamma * self.out_ + self.beta
-#         else:
-#             scale = self.gamma / np.sqrt(self.running_var + self.eps)
-#             out = inp * scale + (self.beta - self.running_mean * scale)
+        if train:
+            self.sample_mean = np.mean(inp, axis=0)
+            self.sample_var = np.var(inp, axis=0)
+            self.out_ = (inp - self.sample_mean) / np.sqrt(self.sample_var + self.eps)
+            self.running_mean = self.momentum * self.running_mean + (1 - self.momentum) * self.sample_mean
+            self.running_var = self.momentum * self.running_var + (1 - self.momentum) * self.sample_var
+            out = self.gamma.value * self.out_ + self.beta.value
+        else:
+            scale = self.gamma.value / np.sqrt(self.running_var + self.eps)
+            out = inp * scale + (self.beta.value - self.running_mean * scale)
 
-#         return out[...,np.newaxis]
+        return out[...,np.newaxis]
 
-#     def backward(grad):
-#         grad = grad[...,0]
+    def backward(self, grad):
+        grad = grad[...,0]
 
-#         N = self.prev_inp.shape[0]
-#         dout_ = self.gamma * grad
-#         dvar = np.sum(dout_ * (self.prev_inp - self.sample_mean) * -0.5 * (self.sample_var + self.eps) ** -1.5, axis=0)
-#         dx_ = 1 / np.sqrt(self.sample_var + self.eps)
-#         dvar_ = 2 * (self.prev_inp - self.sample_mean) / N
+        N = self.prev_inp.shape[0]
+        dout_ = self.gamma.value * grad
+        dvar = np.sum(dout_ * (self.prev_inp - self.sample_mean) * -0.5 * (self.sample_var + self.eps) ** -1.5, axis=0)
+        dx_ = 1 / np.sqrt(self.sample_var + self.eps)
+        dvar_ = 2 * (self.prev_inp - self.sample_mean) / N
 
-#         di = dout_ * dx_ + dvar * dvar_
-#         dmean = -1 * np.sum(di, axis=0)
-#         dmean_ = np.ones_like(self.prev_inp) / N
+        di = dout_ * dx_ + dvar * dvar_
+        dmean = -1 * np.sum(di, axis=0)
+        dmean_ = np.ones_like(self.prev_inp) / N
 
-#         dx = di + dmean * dmean_
-#         self.d_gamma = np.sum(grad * self.out_, axis=0)
-#         self.d_beta = np.sum(grad, axis=0)
+        dx = di + dmean * dmean_
+        self.gamma.dvalue = np.sum(grad * self.out_, axis=0)
+        self.beta.dvalue = np.sum(grad, axis=0)
 
-#         return dx[...,np.newaxis]
+        return dx[...,np.newaxis]
 
 
 
@@ -165,12 +171,12 @@ def MSE_loss(pred, target):
     """
 
     Args:
-        pred.shape = (batch, n_featuers, 1)
-        target.shape = (batch, n_featuers, 1)
+        pred.shape = (batch, n_features, 1)
+        target.shape = (batch, n_features, 1)
     
     Returns:
         loss (float)
-        gred.shape = (batch, n_featuers, 1)
+        gred.shape = (batch, n_features, 1)
 
     """
     loss = np.mean((pred - target) ** 2).item()
@@ -182,12 +188,12 @@ def cross_entropy_loss(pred, target):
     """
 
     Args:
-        pred.shape = (batch, n_featuers(n_kinds), 1)
-        target.shape = (batch, n_featuers(n_kinds), 1)
+        pred.shape = (batch, n_features(n_kinds), 1)
+        target.shape = (batch, n_features(n_kinds), 1)
     
     Returns:
         loss (float)
-        gred.shape = (batch, n_featuers(n_kinds), 1)
+        gred.shape = (batch, n_features(n_kinds), 1)
 
     """
     loss = target * np.log(pred) + (1-target) * np.log(1-pred)
@@ -263,35 +269,37 @@ class SoftmaxLayer():
 
 class BGD():
     def __init__(self, lr):
+        self.maintaining_params = []
         self.lr = lr
     
-    def link_layers_params(self, layers):
-        self.maintaining_layers = [layer for layer in layers if isinstance(layer, FullConnectedLayer)]
+    def link_parameters(self, layers):
+        for layer in layers:
+            self.maintaining_params.extend([param for param in vars(layer).values()if isinstance(param, Parameter)])
 
     def step(self):
-        for layer in self.maintaining_layers:
-            layer.weight -= layer.d_weight * self.lr
-            layer.bias -= layer.d_bias * self.lr
+        for param in self.maintaining_params:
+            param.value -= self.lr * param.dvalue
 
 
 class Adam():
     def __init__(self, lr=0.001, beta1=0.9, beta2=0.999, epislon=1e-8):
+        self.maintaining_params = []
         self.lr = lr
         self.beta1 = beta1
         self.beta2 = beta2
         self.epislon = epislon
 
-    def link_layers_params(self, layers):
-        self.maintaining_layers = [layer for layer in layers if isinstance(layer, (FullConnectedLayer, ))]
+    def link_parameters(self, layers):
+        for layer in layers:
+            self.maintaining_params.extend([param for param in vars(layer).values()if isinstance(param, Parameter)])
         self.t = 0
-        self.m= [0.] * (2 * len(self.maintaining_layers))
-        self.v = [0.] * (2 * len(self.maintaining_layers))
+        self.m= [0.] * len(self.maintaining_params)
+        self.v = [0.] * len(self.maintaining_params)
     
     def step(self):
         self.t += 1
-        for i, layer in enumerate(self.maintaining_layers):
-            layer.weight -= self.calculate_delta(layer.d_weight, i*2)
-            layer.bias -= self.calculate_delta(layer.d_bias, i*2 + 1)
+        for idx, param in enumerate(self.maintaining_params):
+            param.value -= self.calculate_delta(param.dvalue, idx)
 
     def calculate_delta(self, grad, idx):
         self.m[idx] = self.beta1 * self.m[idx] + (1 - self.beta1) * grad
@@ -313,7 +321,7 @@ def load_network(path):
         return pickle.load(f)
 
 def one_hot_encode(labels, n_types):
-    return np.eye(n_labels)[labels]
+    return np.eye(n_types)[labels]
 
 def one_hot_decode(pred):
     return np.argmax(pred, axis=1)
